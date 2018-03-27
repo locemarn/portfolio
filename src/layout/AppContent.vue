8a6d4a9fc3024bce9de2378ef6207cb17e27fc54
locemarn <marcelon27@gmail.com>
2018-03-27 18:57:39 -0300

    ajustes

--- src/layout/AppContent.vue
+++ src/layout/AppContent.vue
@@ -7,14 +7,13 @@
         <div class="row">
             <div id="sobre" class="media col-xs-12 col-md-12 col-sm-12">
                 <div class="col-xs-6 col-md-8 col-sm-6" id="descricao">
-                    <p>Estudante do 8º semestre de Engenharia da Computação, em busca da primeira oportunidade na área de Tecnologia da Informação. Além da universidade, estudo por conta, JavaScript, VueJS e Laravel com PHP.</p>
-                    <p>Estudando para ser um fullstack developer, já possuindo conhecimentos básicos em PHP, JavaScript, NodeJS, VueJS, MySql, Mongodb, Firebase e Git.</p>
+                    <p>Estudante do 8º semestre de Engenharia da Computação, em busca da primeira oportunidade na área de Tecnologia da Informação. Além da universidade, estudo por conta, JavaScript, NodeJS e Laravel com PHP.</p>
                     <div  id="linha-header">
                     </div>
-                    Além disso gosto de me manter em movimento, sempre praticando alguma atividade física, como Jiu-Jitsu, corrida, natação ou qualquer outro esporte que faça o corpo e mente trabalharem juntos! :)
+                    <p>Gosto de me manter em movimento, sempre praticando alguma atividade física, como Jiu-Jitsu, corrida, natação ou qualquer outro esporte que faça o corpo e mente trabalharem juntos! :)</p>
                 </div>
                 <div class="align-self-center col-xs-4 col-md-4 col-sm-4">
-                    <img src="../assets/me.jpg" alt="Eu" id="image-eu">
+                    <img src="../assets/me.jpg" alt="Eu" id="image-eu" class="img-fluid img-thumbnail">
                 </div>
             </div>
         </div>
@@ -26,10 +25,10 @@
                 <h4 class="text-center">PROJETOS</h4>
             </div>
 
-            <div class="col-md-10 offset-md-1 col-sm-8 offset-sm-2">
+            <div class="col-md-10 offset-md-1 col-sm-12 offset-sm-2">
               <!-- CICLO DE PROJETOS -->
               <div class="media" style="margin-top:5%;" id="port">
-                  <img class="align-self-center mr-3" data-toggle="modal" data-target="bd-example-modal-lg" src="../assets/ecommerce01.png" id="img-port" alt="ecommerce imagem">
+                  <img class="align-self-center mr-3 img-fluid img-thumbnail" data-toggle="modal" data-target="bd-example-modal-lg" src="../assets/ecommerce01.png" id="img-port" alt="ecommerce imagem">
                   <div class="media-body">
                       <h5 class="mt-0">E- commerce</h5>
                       <p>E-commerce feito para um projeto da faculdade, onde penso em dar continuidade. Feito em PHP, Slim Framework e MySql, em acompanha-
@@ -44,11 +43,11 @@
                       <p>"Game" feito como exercício do curso <a href="https://www.udemy.com/vuejs-2-the-complete-guide/learn/v4/overview" target="_blank">Vue JS 2 - The Complete Guide</a>, da plataforma <a href="https://www.udemy.com" target="_blank">Udemy.</a></p>
                       <p>Curso de aprendizado da Biblioteca <a href="https://vuejs.org/" target="_blank">VueJS</a>. <a href="https://github.com/locemarn/MonsterSlayerGame" target="_blank"><i class="fa fa-github" aria-hidden="true" id="social-l"></i> GitHub</a>.</p>
                   </div>
-                  <img class="ml-3" src="../assets/monster.png" alt="Monster Slayer Game" id="img-port">
+                  <img class="ml-3 img-fluid img-thumbnail" src="../assets/monster.png" alt="Monster Slayer Game" id="img-port">
               </div>
 
               <div class="media" style="margin-top:5%;" id="port">
-                  <img class="align-self-center mr-3" src="../assets/blogLaravel.png" id="img-port" alt="Laravel Blog">
+                  <img class="align-self-center mr-3 img-fluid img-thumbnail" src="../assets/blogLaravel.png" id="img-port" alt="Laravel Blog">
                   <div class="media-body">
                       <h5 class="mt-0">Blog com Laravel</h5>
                       <p>Blog feito acompanhado da série <a href="https://www.youtube.com/watch?v=rzxNpfiLHSg&list=PLwAKR305CRO_cukAejlt5kz3pTtDMW_Cd" target="_blank">DevMarketer</a>, com as tecnologias: Laravel e VueJS. Foi realizado a parte administrativa do blog, para acrescentar as notícias e ainda não terminado.</p>
@@ -62,7 +61,7 @@
                       <p>Blog realizado em acompanhamento da série <a href="https://www.youtube.com/watch?v=FXY1UyQfSFw&list=PL55RiY5tL51qxUbODJG9cgrsVd7ZHbPrt" target="_blank">Academind</a>, com as tecnologias: VueJS, Vuetify e FireBase. O acompanhamento da série foi feito para aprendizado básico das tecnologias citadas.</p>
                       <p class="mb-0">Projeto no Bitbucket em particular, porém em breve no GitHub para acesso pΰublico.</p>
                   </div>
-                  <img class="ml-3" src="../assets/DevMeetup.png" alt="Dev Meetup Blog" id="img-port">
+                  <img class="ml-3 img-fluid img-thumbnail" src="../assets/DevMeetup.png" alt="Dev Meetup Blog" id="img-port">
               </div>
 
             </div>
@@ -110,10 +109,10 @@
           </div>
           <!-- <div class="col-1"></div> -->
           <div class="col-5" id="descricao-contato">
-            <p>Olá, tudo bem??</p>
-            <p>Quer entrar em contato comigo?? Seja para solicitar mais informações sobre minha pessoa, minha experiência, conversar ou filosofar sobre a vida. Sinta-se livre para deixar mensagem a qualquer momento.</p>
-            <p>Prometo responder!</p>
-            <p>Você pode também pegar meu currículo no botão abaixo.</p>
+            <p>Olá, tudo bem??
+            Quer entrar em contato comigo?? Seja para solicitar mais informações sobre minha pessoa, minha experiência, conversar ou filosofar sobre a vida. Sinta-se livre para deixar mensagem a qualquer momento.</p>
+            <!-- <p>Prometo responder!</p> -->
+            <!-- <p>Você pode também pegar meu currículo no botão abaixo.</p> -->
 
             <a href="https://drive.google.com/file/d/1tNcCZ0eZ6oHFTRBsgMT-GfC8Y88gqMRZ/view?usp=sharing"  target="_blank" class="btn btn-outline-dark  " role="button" aria-pressed="true" style="margin-bottom: 10px">Currículo</a>
 
@@ -171,13 +170,11 @@ export default {
   color: #717878;
   margin-top: 5.2%;
   width: 100%;
-  /* background: #eeeeee; */
 }
 
 #sobre {
   width: auto;
   height: auto;
-  /* box-shadow: 1px 1px 20px #606868; */
   background: #e6ffde;
   padding-top: 3%;
   padding-bottom: 7%;
@@ -203,11 +200,10 @@ export default {
 
   /* border: 3px solid #e2e5e5; */
 #image-eu {
-  width: 300px;
-  height: 300px;
+  width: 200px;
+  height: 200px;
   border-radius: 50%;
-  box-shadow: 1px 1px 20px #606868;
-  margin-left: 7%;
+  box-shadow: 0 0 5px #606868;
 }
 
 /* projetos */
@@ -221,7 +217,7 @@ export default {
 
 #img-port {
   max-width: 40%;
-  height: 200px;
+  height: auto;
 }
 
 /* CONTATO */
